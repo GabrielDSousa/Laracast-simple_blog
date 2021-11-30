a8153f4f1740685c376e75c9eb92cb48a3be3b50
Gabriel <gabrielramos.email@gmail.com>
2021-11-30 20:21:12 -0300

    Lesson 7

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,31 +9,10 @@
     <title>Simple blog</title>
 </head>
 <body>
-<article>
-    <h1><a href="/post/my-first-post">My first post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo inventore, non? Accusamus animi aperiam
-        blanditiis consectetur ducimus eaque, eveniet fuga id ipsam ipsum iste, itaque laudantium minus obcaecati saepe
-        voluptatibus.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/post/my-second-post">My second post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo inventore, non? Accusamus animi aperiam
-        blanditiis consectetur ducimus eaque, eveniet fuga id ipsam ipsum iste, itaque laudantium minus obcaecati saepe
-        voluptatibus.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/post/my-third-post">My third post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo inventore, non? Accusamus animi aperiam
-        blanditiis consectetur ducimus eaque, eveniet fuga id ipsam ipsum iste, itaque laudantium minus obcaecati saepe
-        voluptatibus.
-    </p>
-</article>
+    @foreach ($posts as $post)
+        <article>
+            {!! $post !!}
+        </article>
+    @endforeach
 </body>
 </html>
