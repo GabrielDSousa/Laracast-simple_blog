622bc763da4b2353b8f63529ca29960958a39348
Gabriel <gabrielramos.email@gmail.com>
2021-11-30 19:16:21 -0300

    Second lesson

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>Simple blog</title>
+</head>
+<body>
+<article>
+    <h1><a href="/post">My first post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo inventore, non? Accusamus animi aperiam
+        blanditiis consectetur ducimus eaque, eveniet fuga id ipsam ipsum iste, itaque laudantium minus obcaecati saepe
+        voluptatibus.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My second post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo inventore, non? Accusamus animi aperiam
+        blanditiis consectetur ducimus eaque, eveniet fuga id ipsam ipsum iste, itaque laudantium minus obcaecati saepe
+        voluptatibus.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My third post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo inventore, non? Accusamus animi aperiam
+        blanditiis consectetur ducimus eaque, eveniet fuga id ipsam ipsum iste, itaque laudantium minus obcaecati saepe
+        voluptatibus.
+    </p>
+</article>
+</body>
+</html>
